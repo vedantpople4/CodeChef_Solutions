3a0b8347b2cbcb121b0b87340fdb0fd012b2352f
Vedant pople <vedantpople4@gmail.com>
2021-01-02 21:13:29 +0530

    Add files via upload

---
+++ SMPAIR.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define db double
+#define pb push_back
+#define mpr make_pair
+#define andl "\n"
+#define f first
+#define s second
+#define mset(x,y) memset(x,y,sizeof(x))
+#define fr(i,h,n) for(long long int i=h;i<n;i++)
+#define trace(it,x) for(auto it = (x).begin(); it != (x).end(); it++)
+#define mod 1000000007
+#define fastio ios::sync_with_stdio (false); cin.tie (0); cout.tie (0);
+#define runtime cerr<< '\n' << "Time elapsed :" << clock() * 1000.0 / CLOCKS_PER_SEC << " ms\n" ;
+using namespace std;
+     
+       
+int main()
+{
+    	fastio
+    	ll t;
+    	cin>>t;
+    	while(t--){
+    		ll n;
+    		cin>>n;
+    		ll a[n];
+    		fr(i,0,n) cin>>a[i];
+    		sort(a,a+n);
+    		cout<<a[0]+a[1]<<andl;
+    	
+    	}
+    	
+    	runtime;
+    	
+    	
+    	
+}
