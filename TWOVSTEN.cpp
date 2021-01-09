2b87843e04d558d5e5f960b95f7fbc0c12c2709f
Vedant pople <vedantpople4@gmail.com>
2021-01-09 23:05:03 +0530

    Add files via upload

---
+++ TWOVSTEN.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define db double
+#define pb push_back
+#define mpr make_pair
+#define andl "\n"
+#define f first
+#define s second
+#define mset(x,y) memset(x,y,sizeof(x))
+#define fr(i,h,n) for(long long int i=h;i<n;i++)
+#define trace(it,x) for(auto it = (x).begin(); it != (x).end(); it++)
+#define mod 1000000007
+#define fastio ios::sync_with_stdio (false); cin.tie (0); cout.tie (0);
+#define runtime cerr<< '\n' << "Time elapsed :" << clock() * 1000.0 / CLOCKS_PER_SEC << " ms\n" ;
+using namespace std;
+     
+       
+int main()
+{
+    	fastio
+    	ll t;
+    	cin>>t;
+    	while(t--){
+    	ll n;
+    	cin>>n;
+    	ll ans=0;
+    	if(n%10==0) cout<<0<<endl;
+    	else if (n%10==5) cout<<1<<endl;
+    	else cout<<-1<<endl;
+    	}
+    	
+    	runtime;
+}
