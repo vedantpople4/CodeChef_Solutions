bc561bfb21b20925b1c9357fd6e8952b48a4a7e0
Vedant pople <vedantpople4@gmail.com>
2021-02-14 22:13:32 +0530

    Create PRJOKE1.cpp

---
+++ PRJOKE1.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	int arr[41];
+	arr[0]=0;
+	for(int i=1;i<41;i++)
+	{
+       arr[i]=i^arr[i-1];
+	}
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    for(int i=0;i<n;i++)
+	    {
+	        int a,b;
+	        cin>>a>>b;
+	    }
+	    cout<<arr[n]<<endl;
+	}
+	return 0;
+}
+
