cb04dc1486d4934c707f18e61d909fda686fb1f7
Vedant pople <vedantpople4@gmail.com>
2021-08-06 16:01:45 +0530

    Create CHCOINSG.cpp

---
+++ CHCOINSG.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ld long double
+using namespace std;
+int main() {
+int t;
+cin>>t;
+while(t--)
+{
+int n;
+cin>>n;
+
+if(n%6==0)cout<<"Misha";
+else cout<<"Chef";
+  
+cout<<'\n';
+}
+return 0;
+}
