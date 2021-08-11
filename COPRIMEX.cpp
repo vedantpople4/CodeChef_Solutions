92d2b301dfe2d92dcc644b139a499043735963f0
Vedant pople <vedantpople4@gmail.com>
2021-08-11 21:15:14 +0530

    Create COPRIMEX.cpp

---
+++ COPRIMEX.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long int l,r;
+	    cin>>l>>r;
+	    cout<<1000003<<endl;
+	}
+	return 0;
+}
