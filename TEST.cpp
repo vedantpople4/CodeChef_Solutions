109cb185ff7cef0f7e07f42710fcdefea999590d
Vedant pople <vedantpople4@gmail.com>
2022-03-11 18:52:49 +0530

    Odd Sum

--- TEST.cpp
+++ TEST.cpp
@@ -1,31 +1,59 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 #define ll long long int
-#define db double
-#define pb push_back
-#define mpr make_pair
-#define andl "\n"
-#define f first
-#define s second
-#define mset(x,y) memset(x,y,sizeof(x))
-#define fr(i,h,n) for(long long int i=h;i<n;i++)
-#define trace(it,x) for(auto it = (x).begin(); it != (x).end(); it++)
-#define mod 1000000007
-#define fastio ios::sync_with_stdio (false); cin.tie (0); cout.tie (0);
-#define runtime cerr<< '\n' << "Time elapsed :" << clock() * 1000.0 / CLOCKS_PER_SEC << " ms\n" ;
 using namespace std;
-     
+#define fo(i,n) for(int i=0;i<n;i++)
+#define foc(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define rep(i,l,r) for(i=l;i<=r;i++)
+#define rrep(i,l,r) for(i=l;i>=r;i--)
+#define erep(i,arr) for(auto i:arr)
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define inf INT_MAX
+#define mod 1000000007
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define tc ll t; cin>>t; while(t--)
+
+vector<ll> prime;
+
+void solve(){
+
+
+	
+}  
        
 int main()
 {
-    	fastio
-    	ll t;
-    	do{
-    		cin>>t;
-    		if(t!=42) cout<<t<<endl;
-		}
-		while(t!=42);
-    	
-    	runtime;
+    	fastio;
+		tc solve()
+		return 0;
     	
     	
     	
---
+++ The_Army.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#define ll long long int
+using namespace std;
+#define fo(i,n) for(int i=0;i<n;i++)
+#define foc(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define rep(i,l,r) for(i=l;i<=r;i++)
+#define rrep(i,l,r) for(i=l;i>=r;i--)
+#define erep(i,arr) for(auto i:arr)
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define inf INT_MAX
+#define mod 1000000007
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define tc ll t; cin>>t; while(t--)
+
+vector<ll> prime;
+
+void solve(){
+    //Write your code here
+    ll n, l, k;
+    cin>>n>>l;
+    ll a[l];
+    for(int i=0; i<l; i++)
+        cin>>a[i];
+    sort(a, a+l);
+    for(int i=0; i<n; i++){
+        cout<<(abs(a[0]-i)>abs(a[l-1]-i)?abs(a[0]-i):abs(a[l-1]-i))<<" ";
+    }
+    cout<<"\n";
+}
+
+int main(){
+    fastio;
+    tc solve();
+    return 0;
+}
+
